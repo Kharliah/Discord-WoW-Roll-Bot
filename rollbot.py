fd43ac3944f0eee1ada139f981607d2d09dfcb84
Kharliah <105899626+Kharliah@users.noreply.github.com>
2023-04-24 13:55:12 +1000

    Update rollbot.py
    Added comments

--- rollbot.py
+++ rollbot.py
@@ -14,11 +14,11 @@ async def on_message(message):
         return
  
     if message.content.startswith('/roll'): #change your prefix here if you don't want forward slash as the option
-        number = message.content.split()
-        num = int(number[1])
-        result = str(random.randint(1,num))
-        #print(message.author.display_name)
+        number = message.content.split() #split string into an array
+        num = int(number[1]) #get the 2nd array value (first will be 'roll') 
+        result = str(random.randint(1,num)) #get a random number between 1 and the users number
+        #print(message.author.display_name) 
         #print(result)
-        await message.channel.send(str(message.author.display_name + ' rolls ' + result + ' (1-' + str(num) + ')' ))
+        await message.channel.send(str(message.author.display_name + ' rolls ' + result + ' (1-' + str(num) + ')' )) #post to the channel the result
                                        
 client.run(<your client token here>)
